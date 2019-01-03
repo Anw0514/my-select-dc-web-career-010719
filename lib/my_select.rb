e502ff95d56e94015c8c2e3e9830a4c64bf3cde5
Andrea Williams <anw0514@gmail.com>
2019-01-03 15:29:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  newar = []
+  while i < array.length 
+    yield(array[i])
+    if yield(array[i]) == true
+      newar << yield(array[i])
+    end
+    i = i + 1
+  end
 end
